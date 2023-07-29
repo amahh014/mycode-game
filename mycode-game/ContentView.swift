06ff68c54a7d3eb4437f0c61f6bb14dc78fa8eae
Nizam Mamedov <nizammamedovv@gmail.com>
2023-07-29 20:57:33 +0200

    mycode

--- mycode-game/ContentView.swift
+++ mycode-game/ContentView.swift
@@ -8,6 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var options = ["🪨", "📄", "✂️"]
+    @State private var NToWin = Bool.random()
+    @State private var rounds = 0
+    
+    let compNo = Int.random(in: 0...2)
+    var toWin : String {
+        if options[compNo] == "🪨" {
+            return "📄"
+        } else if options[compNo] == "📄" {
+            return "✂️"
+        } else {
+            return "🪨"
+        }
+    }
+    
+    @State private var showsAlert = false
+    @State private var titleAlert = ""
+    @State private var wasCorrect = false
+    @State private var score = 0
+    @State private(set) var highScore = 0
+    @State private var gameEnded = false
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")

844b61e32d1e4058dd90015852d358416733d596
Nizam Mamedov <nizammamedovv@gmail.com>
2023-07-29 16:07:39 +0200

    Initial Commit

---
+++ mycode-game/mycode_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  mycode_gameApp.swift
+//  mycode-game
+//
+//  Created by Nizam Mamedzadeh on 29.07.23.
+//
+
+import SwiftUI
+
+@main
+struct mycode_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

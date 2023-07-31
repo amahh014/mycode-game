148e65ffb104bc650e8ba159c358dffdeef63a48
Nizam Mamedov <nizammamedovv@gmail.com>
2023-07-31 19:39:12 +0200

    Design Changes
    Design like in figma

--- mycode-game/ContentView.swift
+++ mycode-game/ContentView.swift
@@ -31,18 +31,142 @@ struct ContentView: View {
     @State private var gameEnded = false
     
     var body: some View {
-        VStack {
-            Image(systemName: "globE")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+            VStack {
+                Spacer()
+                Text("Rock Paper Scissors")
+                    .padding()
+                    .scaledToFit()
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                Text("Computer chose...")
+                    .padding()
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                    Text(options[compNo])
+                        .font(.largeTitle)
+                }
+                .frame(width: 208, height: 68)
+                .foregroundColor(.white)
+                Text("You must ....")
+                    .padding()
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                    Text(NToWin ? "Win" : "Lose")
+                        .foregroundColor(.white)
+                        .padding()
+                        .font(.largeTitle)
+                }
+                .frame(width: 208, height: 68)
+                .foregroundColor(NToWin ? .green : .red)
+                Text("What you should chose")
+                    .padding()
+                HStack {
+                    Spacer()
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 5)
+                        Button("🪨") {
+                            let userOption = "🪨"
+                            chosen(user: userOption)
+                        } .foregroundColor(.yellow)
+                    }
+                    .frame(width: 65, height: 60)
+                    .foregroundColor(.white)
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 5)
+                        Button("📄") {
+                            let userOption = "📄"
+                            chosen(user: userOption)
+                        } . foregroundColor(.black)
+                    }
+                    .frame(width: 65, height: 60)
+                    .foregroundColor(.red)
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 5)
+                        Button("✂️") {
+                            let userOption = "✂️"
+                            chosen(user: userOption)
+                        } .foregroundColor(.black)
+                    }
+                    .frame(width: 65, height: 60)
+                    .foregroundColor(.white)
+                Spacer()
+                }
+                Spacer()
+                HStack {
+                    Text("Score: \(score) / \(rounds)")
+                        .padding(20)
+                    VStack {
+                        Text("High Score: \(highScore)")
+                        Button("Reset") {
+                            highScore = 0
+                        } .foregroundColor(.yellow)
+                    }
+                } .alert(titleAlert, isPresented: $showsAlert) {
+                    Button("Next", action: nextQuestion)
+                } message: {
+                    if wasCorrect == true {
+                        Text("Your score is \(score)")
+                    } else {
+                        Text("try again")
+                    }
+                }.alert("Game Over", isPresented: $gameEnded) {
+                    Button("Restart game", action: gameOver)
+                } message: {
+                    if wasCorrect == true {
+                        Text("Your final socre is \(score)")
+                    } else {
+                        Text("Your final score is \(score)")
+                    }
+                }
+            }
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    func chosen(user: String) {
+        rounds += 1
+        if user == toWin && NToWin == true {
+            titleAlert = "Correct"
+            wasCorrect = true
+            score += 1
+        }
+        if user == toWin && NToWin == false {
+            titleAlert = "Wrong"
+            wasCorrect = false
+        }
+        if user != toWin && NToWin == true {
+            titleAlert = "Wrong"
+            wasCorrect = false
+        }
+        if user != toWin && NToWin == false {
+            titleAlert = "Correct"
+            wasCorrect = true
+            score += 1
+        }
+        if rounds == 10 {
+            gameEnded = true
+        } else {
+            showsAlert = true
+        }
+    }
+    
+    func nextQuestion() {
+        options.shuffle()
+        NToWin = Bool.random()
+    }
+    func gameOver() {
+        nextQuestion()
+        rounds = 0
+        if score > highScore {
+            highScore = score
+        }
+        score = 0
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
